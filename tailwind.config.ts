88f448fe1d63be49d0a9de0c5a74903f73297715
Ahmed Qudsi Ghouse Ali Khan <ahmedqudsi786@gmail.com>
2025-05-26 08:56:56 +0000

    when it goes inside an end it takes way too long to come back from other end

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -91,7 +91,7 @@ export default {
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out',
-        'marquee-to-right': 'marquee-to-right 15s linear infinite',
+        'marquee-to-right': 'marquee-to-right 10s linear infinite',
   		}
   	}
   },
