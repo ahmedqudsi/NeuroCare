a138cb7abcf05fd6c579775c70c7cc4170ddd83b
Omar Syed <oskdon05@gmail.com>
2025-05-29 09:47:55 +0000

    It is in the images file named favicon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     template: `%s | ${siteConfig.appName}`,
   },
   description: siteConfig.description,
-  icons: ['/images/blue-brain-favicon.png'], // Updated favicon path
+  icons: ['/images/favicon.ico'], // Updated favicon path
 };
 
 export default function RootLayout({
