82ee0243afd258e5153367c5fa63387a1c29bdc7
Ahmed Qudsi Ghouse Ali Khan <ahmedqudsi786@gmail.com>
2025-05-29 10:42:22 +0000

    still its not loading..pls..fix the error ..i want my app the way it was

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     template: `%s | ${siteConfig.appName}`,
   },
   description: siteConfig.description,
-  icons: ['/favicon.ico'], // Reverted favicon path
+  icons: ['/favicon.ico'], 
 };
 
 export default function RootLayout({
@@ -30,7 +30,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (<html lang="en" suppressHydrationWarning>
+  return (
+    <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}>
         <ThemeProvider
           attribute="class"
@@ -41,7 +42,7 @@ export default function RootLayout({
           {children}
           <Toaster />
         </ThemeProvider>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </body>
-    </html>);
+    </html>
+  );
 }
